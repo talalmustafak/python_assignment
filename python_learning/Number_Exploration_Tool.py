67ff9526a4109d170f7e4425b35c3a91b90e0d1d
Talal Khan <talalmustafa725@gmail.com>
2024-08-29 16:30:04 +0500

    second commit with minor changes

---
+++ python_learning/Number_Exploration_Tool.py
@@ -0,0 +1,10 @@
+name : str = input("Enter your name: ")
+
+numbers = []
+
+for i in range(1,4): 
+
+first_num = int(input("Enter your first favorite number:"))
+second_num = int(input("Enter your second favorite number:"))
+third_num = int(input("Enter your third favorite number:"))
+
